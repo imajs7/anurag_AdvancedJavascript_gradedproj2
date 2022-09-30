11ad19e583847f82b3a558c37d4345b24d397e3e
imajs7 <anukhg@gmail.com>
2022-09-30 13:00:39 +0530

    login / signup  logic completed

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
-import data from '../data/Data.json' assert { type: "json" };
+let authData = JSON.parse( localStorage.getItem('resume-auth-data') );
 
-if( localStorage.getItem('resume-auth-data') == null ) {
+if( authData == null || authData.status == false ) {
     location.href = 'login.html';
     exit;
 }
+// import data from json file
+import data from '../data/Data.json' assert { type: "json" };
 
 // get resume by index
 const getResume = index => data['resume'][index];
 
-console.log( getResume( 2 ).basics.name );
+// console.log( getResume( 2 ).basics.name );
+
+document.querySelector('#logout').addEventListener( 'click', () => {
+    if( authData.save == true ) {
+        authData.status = false;
+        localStorage.setItem('resume-auth-data', JSON.stringify( authData ) );
+    } else {
+        localStorage.removeItem('resume-auth-data');
+    }
+    location.href = '/login.html';
+} );
--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
-if( localStorage.getItem('resume-auth-data') != null ) {
+let authData = JSON.parse( localStorage.getItem('resume-auth-data') );
+if( authData != null && authData.status == true ) {
     location.href = '/';
     exit;
 }
@@ -6,6 +7,10 @@ if( localStorage.getItem('resume-auth-data') != null ) {
 const switcher = document.querySelector("#switch");
 const signinForm = document.querySelector("#signin-form");
 const signupForm = document.querySelector("#signup-form");
+
+const formSignup = document.querySelector('#formSignup');
+const formSignin = document.querySelector('#formSignin');
+const errorMsg = document.querySelector('.error');
 
 switcher.addEventListener( 'click', () => {
     if( switcher.checked == true ) {
@@ -25,4 +30,66 @@ switcher.addEventListener( 'click', () => {
         signinForm.style.opacity = 1;
         signinForm.style.visibility = 'visible';
     }
+    errorMsg.textContent = '';
 } );
+
+formSignup.addEventListener( 'submit', event => {
+    event.preventDefault();
+
+    if ( authData != null ) {
+        errorMsg.textContent = 'More than one sign up not allowed';
+        formSignup.reset();
+        return;
+    }
+    const usernameSignup = document.querySelector("#username-signup");
+    const passwordSignup = document.querySelector("#password-signup");
+    const passwordConfirm = document.querySelector("#password-confirm");
+    const acceptance = document.querySelector("#acceptance");
+    if( 
+        usernameSignup.value != '' && 
+        passwordSignup.value != '' && 
+        passwordConfirm.value != '' && 
+        passwordSignup.value == passwordConfirm.value &&
+        acceptance.checked == true
+    ) {
+        authData = {
+            username: usernameSignup.value,
+            password: passwordSignup.value,
+            status: false,
+            save: false
+        }
+        localStorage.setItem('resume-auth-data', JSON.stringify( authData ) );
+        errorMsg.textContent = 'Success! Sign in to use the app.';
+        formSignup.reset();
+    } else {
+        errorMsg.textContent = 'Please enter the form details correctly';
+    }
+} );
+
+formSignin.addEventListener( 'submit', event => {
+    event.preventDefault();
+    const usernameSignin = document.querySelector("#username-signin");
+    const passwordSignin = document.querySelector("#password-signin");
+    if( authData == null ) {
+        errorMsg.textContent = 'No account created. Please signup to use this app';
+        formSignin.reset();
+        return;
+    }
+    if(
+        usernameSignin.value != '' && 
+        passwordSignin.value != '' && 
+        usernameSignin.value == authData.username &&
+        passwordSignin.value == authData.password
+    ) {
+        authData.status = true;
+        authData.save = keepLoggedIn.checked ? true : false;
+        localStorage.setItem('resume-auth-data', JSON.stringify( authData ) );
+        location.href = '/';
+    } else {
+        errorMsg.textContent = 'Invalid username/password';
+    }
+} );
+
+const showPassword = () => {
+    errorMsg.innerHTML = `Username = ${authData.username} <br/> Password = ${authData.password}`;
+};
